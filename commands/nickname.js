757e3a0d18c1f2b607d1cf7034b0e43eb34b2fcf
mkanarr <matthewkanarr@gmail.com>
2020-12-16 01:23:28 -0600

    cool names

---
+++ commands/nickname.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: 'nickname',
+    description: 'Change your nickname',
+    args: true,
+    usage: '<nickname>',
+    aliases: ['newname', 'changename', 'name'],
+    execute(message, args) {
+        const member = message.member;
+        const nickName = message.content.match(/\b(?!name\b)\S[^~,]*/)[0];
+
+        member.setNickname(nickName);
+    },
+};
