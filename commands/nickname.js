2da0feb9665a00eb35cf45c923321215ccde9031
mkanarr <matthewkanarr@gmail.com>
2020-12-17 17:49:50 -0600

    fixed regex

--- commands/nickname.js
+++ commands/nickname.js
@@ -6,7 +6,7 @@ module.exports = {
     aliases: ['newname', 'changename', 'name'],
     execute(message, args) {
         const member = message.member;
-        const nickName = message.content.match(/\b(?!name\b)\S[^~,]*/)[0];
+        const nickName = message.content.match(/\b(?!name|changename|newname|nickname\b)\S[^~,]*/)[0];
 
         member.setNickname(nickName);
     },
