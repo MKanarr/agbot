85d9a38f155ab28a7cd1797dd4c58f35c6a87ff3
mkanarr <matthewkanarr@gmail.com>
2020-12-19 03:07:05 -0600

    added regex for message checking

--- commands/channel.js
+++ commands/channel.js
@@ -5,15 +5,41 @@ module.exports = {
     description: 'Create new channel',
     args: true,
     usage: '<channel_name> <text/voice>',
-    aliases: ['channel', 'add channel'],
+    aliases: ['chan', 'addchan', 'newchan'],
     execute(message, args) {
 
         if (!message.member.hasPermission('ADMINISTRATOR')) {
           return message.channel.send('Nice try.');
         }
 
-        const channelName = args[0];
-        const channelType = args[1];
+        // parses out command name & prefix
+        var re = new RegExp(/\b(?!channel|addchan|newchan|chan\b)\S[^~,]*/);
+        // parses out channel type
+        var reg = new RegExp(/voice$|text$/i);
+        // parses out channel name
+        var regi = new RegExp(/[^~,]*\S(?= text$| voice$)/i);
+
+        var msg = message.content;
+
+        console.log(message.content);
+
+        // msg w/o cmd name & prefix
+        var parsedMsg = msg.match(re)[0];
+
+        // check if channel type is provided
+        if (parsedMsg.match(reg) === null) {
+          console.log('channel type not provided');
+          return message.channel.send(`${message.author}, please provide a channel type: text or voice`);
+        }
+
+        var channelType = parsedMsg.match(reg)[0].toLowerCase();
+        var channelName = parsedMsg.match(regi)[0];
+
+        console.log(channelType);
+        console.log(channelName);
+
+        // const channelName = args[0];
+        // const channelType = args[1];
 
         message.guild.channels.create(channelName, {
             type: channelType,
